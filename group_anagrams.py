906f55b626833c597f5fdcb1a660f25e44c1baaa
Alex Monson <monson.alex@rocketmail.com>
2026-08-02 19:53:54 -0700

    added solution for group anagrams

---
+++ group_anagrams.py
@@ -0,0 +1,36 @@
+def groupAnagrams(strs):
+    words = {}
+    key = ()
+    
+    for word in strs:
+        count = [0] * 26
+
+        for letter in word:
+            index = ord(letter) - ord("a")
+            count[index] += 1
+        
+       
+        
+        key = tuple(count)
+        if key in words:
+            words[key].append(word)
+        else:
+            words[key] = [word]
+        
+    return words.values()
+
+        
+    
+        
+
+
+
+
+
+
+
+
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"])) #[["bat"],["nat","tan"],["ate","eat","tea"]]
+print(groupAnagrams([""])) # [[""]]
+print(groupAnagrams(["a"])) # ["a"]]
